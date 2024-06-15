615b3a203f9381c043a31c50a4d30db689c3c30c
ggobsari <ggobsari123@gmail.com>
2024-06-15 16:11:13 +0900

    Initial commit

---
+++ src/main/java/com/hollroom/HollroomApplication.java
@@ -0,0 +1,13 @@
+package com.hollroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hollroom/HollroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
