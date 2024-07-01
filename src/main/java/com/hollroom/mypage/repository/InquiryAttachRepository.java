ea7a5257f25ed355a994eec51272fd165668081a
PKW-TEST <alpamon615@gmail.com>
2024-07-02 01:22:55 +0900

    feat:문의하기(목록/작성/파일업로드/상세보기/파일불러오기)

---
+++ src/main/java/com/hollroom/mypage/repository/InquiryAttachRepository.java
@@ -0,0 +1,11 @@
+package com.hollroom.mypage.repository;
+
+import com.hollroom.community.domain.entity.AttachFileEntity;
+import com.hollroom.mypage.domain.entity.InquiryAttatchEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InquiryAttachRepository extends JpaRepository<InquiryAttatchEntity,Long> {
+    List<InquiryAttatchEntity> findByPostId(Long postId);
+}
---
+++ src/main/java/com/hollroom/mypage/service/InquiryService.java
@@ -0,0 +1,24 @@
+package com.hollroom.mypage.service;
+
+import com.hollroom.mypage.domain.dto.InquiryAttatchDTO;
+import com.hollroom.mypage.domain.dto.InquiryDTO;
+import com.hollroom.mypage.domain.entity.InquiryEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface InquiryService {
+    //문의글 목록
+    Page<InquiryDTO> getInquiries(Pageable pageable);
+    //문의글 업로드
+    InquiryEntity saveInquiry(InquiryDTO inquiryDTO) throws Exception;
+    //첨부파일 업로드
+    void saveInquiryFile(List<MultipartFile> files, Long postId) throws Exception;
+
+    //문의글 상세보기
+    InquiryDTO getInquiryById(Long id);
+    //첨부파일 불러오기
+    List<InquiryAttatchDTO> getAttachmentsByPostId(Long postId);
+}
