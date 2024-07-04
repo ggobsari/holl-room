ec5992bd5468c4c1ca02f4a69ed85d76d8913b5c
PKW-TEST <alpamon615@gmail.com>
2024-07-04 19:08:50 +0900

    feat: Inquiry 공용Attach 사용 / InquriyEntity,Repo 삭제

--- src/main/java/com/hollroom/mypage/domain/dto/InquiryAttatchDTO.java
+++ src/main/java/com/hollroom/mypage/domain/dto/InquiryAttatchDTO.java
@@ -1,14 +1,20 @@
 package com.hollroom.mypage.domain.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.hollroom.common.TabType;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
 @Setter
-@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class InquiryAttatchDTO {
-    private Long id;
-    private String fileName;
-    private String filePath;
+    private Long fileId; //첨부파일 아이디
+
+    private TabType tabType; // 탭타입 - 커뮤니티, 룸메이트, 마이페이지...
+    private String fileOriginalName; //원본파일명
+    private String fileStoreName; //저장된 파일명
 }
