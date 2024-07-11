32dbc8e0a5c538a9c113def766431e731e336e4e
PKW-TEST <alpamon615@gmail.com>
2024-07-11 14:05:28 +0900

    fix : loginType 오류로 인한 버그 수정

--- src/main/resources/static/mypage/js/profile.js
+++ src/main/resources/static/mypage/js/profile.js
@@ -47,7 +47,9 @@ document.addEventListener("DOMContentLoaded", function () {
 // 비밀번호 유효성 검사
 function validatePassword() {
     var password = document.getElementById("password") ? document.getElementById("password").value : '';
-    var confirmPassword = document.getElementById("confirmPasswordInput") ? document.getElementById("confirmPassword").value : '';
+    var confirmPassword = document.getElementById("confirmPasswordInput") ? document.getElementById("confirmPasswordInput").value : '';
+    console.log(password);
+    console.log(confirmPassword);
     if (password != confirmPassword) {
         alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
         return false;
